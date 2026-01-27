7ad3b5b2a0a1f883bdbdde4c0539c3bbac587aa2
yongninglei <yl4874@nyu.edu>
2026-01-27 18:15:45 +0100

    edit heudiconv src code to match bids dicom format

--- MR_pipelines/00_dicom_to_nifti/src_heudiconv_step1.sh
+++ MR_pipelines/00_dicom_to_nifti/src_heudiconv_step1.sh
@@ -19,15 +19,6 @@
 # or you can also use the qsub code to run them by inputing variables
 
 
-# basedir=/bcbl/home/public/Gari/VOTCLOC/VSS
-# dicom_dirname=dicom
-# outputdir=$basedir/BIDS
-# sing_path=/bcbl/home/public/Gari/singularity_images
-
-# sample code to run without ses
-#singularity run --bind /bcbl/home/public/Gari/VOTCLOC/main_exp:/base --bind /bcbl:/bcbl --bind /export:/export /bcbl/home/public/Gari/singularity_images/heudiconv_1.3.2.sif -d /base/dicom/sub-{subject}/*/*/*/*/* -s 01 02 03 04 05 06 08 -o /bcbl/home/public/Gari/VOTCLOC/main_exp/raw_nifti -f convertall -c none -g all --overwrite > ~/public/Gari/VOTCLOC/main_exp/raw_nifti/log_heudiconv/redo_heudiconv-and-using-subonly/all.log 2> ~/public/Gari/VOTCLOC/main_exp/raw_nifti/log_heudiconv/redo_heudiconv-and-using-subonly/all.err
-
-
 module load apptainer/latest
 echo "Now the singularity is loaded, it is: "
 module list
@@ -40,7 +31,7 @@ cmd="singularity run --cleanenv --no-home --containall \
 	    	--bind /bcbl:/bcbl \
 			--bind /export:/export \
         	${sing_path} \
-			-d ${dcm_dir}/sub-{subject}_ses-{session}/*/*.dcm \
+			-d ${dcm_dir}/sub-{subject}/ses-{session}/*/*.dcm \
 	    	-s ${sub} \
 			-ss ${ses} \
 			-o ${outputdir} \
